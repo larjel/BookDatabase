5706760c1c6cd6fcbbe571e7f089e105ed49031e
Lars Jelleryd <larsj99@yahoo.se>
2019-04-11 16:27:58 +0200

    Updates

---
+++ precache-manifest.15809042736c98958f985b0a467ec691.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "851c9c937f161555f52d",
+    "url": "/lab2-js-react-ajax-lars/static/js/runtime~main.8631a453.js"
+  },
+  {
+    "revision": "ef39d30982bf3399d1a3",
+    "url": "/lab2-js-react-ajax-lars/static/js/main.d865b445.chunk.js"
+  },
+  {
+    "revision": "087d495c337084bd0b9e",
+    "url": "/lab2-js-react-ajax-lars/static/js/2.130c8112.chunk.js"
+  },
+  {
+    "revision": "ef39d30982bf3399d1a3",
+    "url": "/lab2-js-react-ajax-lars/static/css/main.0be67861.chunk.css"
+  },
+  {
+    "revision": "efb9891008b0817bc99dd203085524b6",
+    "url": "/lab2-js-react-ajax-lars/index.html"
+  }
+];
